20272bb72e517dc650f230ae116285c06ce2ce82
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-04-26 14:46:50 +0300

    basic setup

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+
+  .get('/', (req, res) => {
+
+    // return json object
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: 1 }));
+
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
