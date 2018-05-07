087034222b6882e3ab2ff1661936d514c2b54380
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-05-07 07:55:01 +0300

    controller

---
+++ controller/tracking.js
@@ -0,0 +1,137 @@
+module.exports = {
+
+
+
+    getTracking : function(req, res){
+      
+      var contactKey = req.query.contactkey;
+
+      // MC API  authentication
+      var FuelSoap = require('fuel-soap');
+      var options = {
+        auth: {
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET
+        },
+        soapEndpoint: process.env.API_URL
+      };
+
+      var SoapClient = new FuelSoap(options);
+
+
+      var tracking = {};
+      var calls = 0;
+      var callsToWait = 3;
+
+      var reqoptions = {
+        filter: {
+          leftOperand: 'SubscriberKey',
+          operator: 'equals',
+          rightOperand: contactKey
+        }
+      };
+
+      // retriece sends
+      SoapClient.retrieve(
+        'SentEvent',
+        ["EventDate","SendID","SubscriberKey"],
+        reqoptions,
+        function( err, response ) {
+          if ( err ) {
+            console.log( err );
+            return;
+          }
+
+          try{
+            var rows =  response.body.Results;
+            for (var i=0; i < rows.length; i++){
+              var row = rows[i];
+              if(!tracking[row['SendID']]){
+                tracking[row['SendID']] = { Sent: [], Click: [], Open:[] };
+              }
+              tracking[row['SendID']]['Sent'].push(row.EventDate);
+            }
+            calls++;
+            if(calls==callsToWait){
+              console.log( tracking );
+              res.setHeader('Content-Type', 'application/json');
+              res.send(JSON.stringify({ tracking }));
+              return ;
+            }
+          }catch(e){
+            console.log(e);
+          }
+        }
+      );
+
+      // retrieve clicks
+      SoapClient.retrieve(
+        'ClickEvent',
+        ["EventDate","SendID","SubscriberKey"],
+        reqoptions,
+        function( err, response ) {
+          if ( err ) {
+            console.log( err );
+            return;
+          }
+
+          try{
+            var rows =  response.body.Results;
+            for (var i=0; i < rows.length; i++){
+              var row = rows[i];
+              if(!tracking[row['SendID']]){
+                tracking[row['SendID']] = { Sent: [], Click: [], Open:[] };
+              }
+              tracking[row['SendID']]['Click'].push(row.EventDate);
+            }
+            calls++;
+            if(calls==callsToWait){
+              console.log( tracking );
+              res.setHeader('Content-Type', 'application/json');
+              res.send(JSON.stringify({ tracking }));
+              return ;
+            }
+          }catch(e){
+            console.log(e);
+          }
+        }
+      );
+
+      // retrieve opens
+      SoapClient.retrieve(
+        'OpenEvent',
+        ["EventDate","SendID","SubscriberKey"],
+        reqoptions,
+        function( err, response ) {
+          if ( err ) {
+            console.log( err );
+            return;
+          }
+
+          try{
+            var rows =  response.body.Results;
+            for (var i=0; i < rows.length; i++){
+              var row = rows[i];
+              if(!tracking[row['SendID']]){
+                tracking[row['SendID']] = { Sent: [], Click: [], Open:[] };
+              }
+              tracking[row['SendID']]['Open'].push(row.EventDate);
+            }
+
+            calls++;
+            if(calls==callsToWait){
+
+              console.log( tracking );
+              res.setHeader('Content-Type', 'application/json');
+              res.send(JSON.stringify({ tracking }));
+              return ;
+            }
+          }catch(e){
+            console.log(e);
+          }
+        }
+      );
+    },
+
+
+}
--- index.js
+++ index.js
@@ -1,157 +1,29 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const trackingController = require('./controller/tracking');
+const bodyParser = require('body-parser');
 
-express()
+const app = express()
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  .get('/', (req, res) => {
+app.get('/tracking', (req, res) => {
+  // APP authentication
+  if( req.query.clientid != process.env.APP_CLIENT_ID || req.query.clientsecret != process.env.APP_CLIENT_SECRET ){
+    console.log('Authentication failed');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ error: 'Authentication failed' }));
+    return ;
+  }
 
-    // APP authentication
-    if( req.query.clientid != process.env.APP_CLIENT_ID || req.query.clientsecret != process.env.APP_CLIENT_SECRET ){
-      console.log('Authentication failed');
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ error: 'Authentication failed' }));
-      return ;
-    }
-
-    // contact key
-    if( !req.query.contactkey ){
-      console.log('Authentication failed');
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({ error: 'ContactKey missing' }));
-      return ;
-    }
-
-    var contactKey = req.query.contactkey;
-
-    // MC API  authentication
-    var FuelSoap = require('fuel-soap');
-    var options = {
-      auth: {
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET
-      },
-      soapEndpoint: process.env.API_URL
-    };
-
-    var SoapClient = new FuelSoap(options);
-
-
-    var tracking = {};
-    var calls = 0;
-    var callsToWait = 3;
-
-    var reqoptions = {
-      filter: {
-        leftOperand: 'SubscriberKey',
-        operator: 'equals',
-        rightOperand: contactKey
-      }
-    };
-
-    // retriece sends
-    SoapClient.retrieve(
-      'SentEvent',
-      ["EventDate","SendID","SubscriberKey"],
-      reqoptions,
-      function( err, response ) {
-        if ( err ) {
-          console.log( err );
-          return;
-        }
-
-        try{
-          var rows =  response.body.Results;
-          for (var i=0; i < rows.length; i++){
-            var row = rows[i];
-            if(!tracking[row['SendID']]){
-              tracking[row['SendID']] = { Sent: [], Click: [], Open:[] };
-            }
-            tracking[row['SendID']]['Sent'].push(row.EventDate);
-          }
-          calls++;
-          if(calls==callsToWait){
-            console.log( tracking );
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify({ tracking }));
-            return ;
-          }
-        }catch(e){
-          console.log(e);
-        }
-      }
-    );
-
-    // retrieve clicks
-    SoapClient.retrieve(
-      'ClickEvent',
-      ["EventDate","SendID","SubscriberKey"],
-      reqoptions,
-      function( err, response ) {
-        if ( err ) {
-          console.log( err );
-          return;
-        }
-
-        try{
-          var rows =  response.body.Results;
-          for (var i=0; i < rows.length; i++){
-            var row = rows[i];
-            if(!tracking[row['SendID']]){
-              tracking[row['SendID']] = { Sent: [], Click: [], Open:[] };
-            }
-            tracking[row['SendID']]['Click'].push(row.EventDate);
-          }
-          calls++;
-          if(calls==callsToWait){
-            console.log( tracking );
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify({ tracking }));
-            return ;
-          }
-        }catch(e){
-          console.log(e);
-        }
-      }
-    );
-
-    // retrieve opens
-    SoapClient.retrieve(
-      'OpenEvent',
-      ["EventDate","SendID","SubscriberKey"],
-      reqoptions,
-      function( err, response ) {
-        if ( err ) {
-          console.log( err );
-          return;
-        }
-
-        try{
-          var rows =  response.body.Results;
-          for (var i=0; i < rows.length; i++){
-            var row = rows[i];
-            if(!tracking[row['SendID']]){
-              tracking[row['SendID']] = { Sent: [], Click: [], Open:[] };
-            }
-            tracking[row['SendID']]['Open'].push(row.EventDate);
-          }
-
-          calls++;
-          if(calls==callsToWait){
-
-            console.log( tracking );
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify({ tracking }));
-            return ;
-          }
-        }catch(e){
-          console.log(e);
-        }
-      }
-    );
-
-
-
-
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  // contact key
+  if( !req.query.contactkey ){
+    console.log('Authentication failed');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ error: 'ContactKey missing' }));
+    return ;
+  }
+  loginContoller.login(req, res)
+})
