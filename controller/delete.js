7765d82e604c2e7d38fe8789d1a44b6a04893fc3
Antti Paukku <anttipaukku@Anttis-MacBook-Pro-2.local>
2018-05-07 08:36:00 +0300

    delete contact

--- controller/delete.js
+++ controller/delete.js
@@ -3,6 +3,34 @@ module.exports = {
   delete : function(req, res){
     console.log('delete')
 
+    var contactKey = req.query.contactkey;
+
+    const RestClient = new FuelRest(optionsRest);
+
+    const optionsRestReq = {
+      uri: '/contacts/v1/contacts/actions/delete?type=keys',
+      headers:{},
+      json: {
+        "ContactTypeId": "0",
+        "values": [contactKey],
+          "DeleteOperationType": "ContactAndAttributes"
+        }
+    };
+
+
+    RestClient.post(optionsRestReq, (err, response) => {
+      if (err) {
+        // error here
+        console.log('error');
+        console.log(err);
+      }
+
+      // will be delivered with 200, 400, 401, 500, etc status codes
+      // response.body === payload from response
+      // response.res === full response from request client
+      console.log('response');
+      console.log(response);
+    });
 
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ Status: 'Deleted' }));
