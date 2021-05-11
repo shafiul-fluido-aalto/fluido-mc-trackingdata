e4e3b83ddecc23881bc7a95a69589ea97db0c6f5
Shafiul-Alam <84014836+Shafiul-Alam@users.noreply.github.com>
2021-05-11 14:07:13 +0300

    Update tracking.js

--- controller/tracking.js
+++ controller/tracking.js
@@ -31,7 +31,7 @@ module.exports = {
           clientId: clientid,
           clientSecret: clientsecret
         },
-        soapEndpoint: process.env['API_URL']+ 'v2/token'
+        soapEndpoint: process.env.API_URL+ 'v2/token'
        
       };
 
