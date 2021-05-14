d6e76eaa90a91b730c38638fab2eb5645215186d
Shafiul Alam <md.2.alam@aalto.fi>
2021-05-14 19:38:26 +0300

    added

--- controller/tracking.js
+++ controller/tracking.js
@@ -24,7 +24,7 @@ module.exports = {
 
       // MC API  authentication
       var FuelSoap = require('fuel-soap');
-        console.log('FuelSoap=> ',FuelSoap);
+       
       var options = {
         auth: {
           clientId: clientid,
